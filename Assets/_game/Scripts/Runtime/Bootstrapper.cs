38e7d59185b04ef0b7ab05da5e951ed3cf349c18
Dmitriy Guliaev <akella-odinoki-volk@yandex.ru>
2021-10-05 22:03:26 +0500

    conflicted file

--- Assets/_game/Scripts/Runtime/Bootstrapper.cs
+++ Assets/_game/Scripts/Runtime/Bootstrapper.cs
@@ -14,23 +14,18 @@ namespace Runtime
             StructureManager.CheckInstance();
             GameData.CheckInstance();
             GameData.Instance.OnEnable();
-<<<<<<< Updated upstream
-=======
-
-
-            List<string> loadedLog = new List<string>();
 
             try
             {
 
                 string path = Directory.GetCurrentDirectory() + "/Mods";
                 string[] fileNames = Directory.GetFiles(path, ".", SearchOption.TopDirectoryOnly);
-                loadedLog.Add($"Path {path}. Files found {fileNames.Length}");
+                //loadedLog.Add($"Path {path}. Files found {fileNames.Length}");
 
 
                 foreach (string assemblyFileName in fileNames)
                 {
-                    loadedLog.Add($"Check {assemblyFileName}");
+                    //loadedLog.Add($"Check {assemblyFileName}");
 
                     if (assemblyFileName.Contains(".dll"))
                     {
@@ -38,44 +33,27 @@ namespace Runtime
                         
                         if (asm != null)
                         {
-                            loadedLog.Add($"Found assamble {asm.GetName().Name}:");
+                            //loadedLog.Add($"Found assamble {asm.GetName().Name}:");
 
                             Type[] types = asm.GetTypes();
 
                             foreach (Type type in types)
                             {
-                                loadedLog.Add(type.FullName);
+                                //loadedLog.Add(type.FullName);
                             }
                         }
                         else
                         {
-                            loadedLog.Add($"Assamble is null - {asm.GetName().Name}:");
+                            //loadedLog.Add($"Assamble is null - {asm.GetName().Name}:");
                         }
                     }
                 }
 
-
-                /*foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
-                {
-                    log.Add(assembly.GetName().Name);
-     
-                }*
-
-                /*using (FileStream stream = new FileStream(Application.dataPath + "/LOG.txt", FileMode.OpenOrCreate))
-                {
-                    stream.Close();
-                }
-                */
-                //log.AddRange(loadedLog);
-
             }
             catch (Exception e)
             {
-                loadedLog.Add(e.ToString());
+                //loadedLog.Add(e.ToString());
             }
-
-            File.WriteAllLines(Application.dataPath + "/LOG.txt", loadedLog);
->>>>>>> Stashed changes
         }
     }
 }
