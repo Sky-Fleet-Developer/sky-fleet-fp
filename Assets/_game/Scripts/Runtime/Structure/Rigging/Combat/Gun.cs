11d91ecb0e2831ec1be2904420b4db77440a83be
Dmitriy Guliaev <Akella-odinoki-volk@yandex.ru>
2024-10-10 01:04:40 +0500

    begin for combat

---
+++ Assets/_game/Scripts/Runtime/Structure/Rigging/Combat/Gun.cs
@@ -0,0 +1,12 @@
+﻿using Core.Structure.Rigging;
+using UnityEngine;
+
+namespace Runtime.Structure.Rigging.Combat
+{
+    public class Gun : BlockWithNode
+    {
+        [SerializeField] private Transform gunpoint;
+        
+        
+    }
+}
