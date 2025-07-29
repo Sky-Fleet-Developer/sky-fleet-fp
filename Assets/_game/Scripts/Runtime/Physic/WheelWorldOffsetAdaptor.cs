ce735f0f44fa0feee9caf6f3dd85e369df1706e5
Dmitriy Guliaev <Akella-odinoki-volk@yandex.ru>
2025-07-30 01:59:57 +0500

    wheels plugin

---
+++ Assets/_game/Scripts/Runtime/Physic/WheelWorldOffsetAdaptor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Core.Game;
+using NWH.WheelController3D;
+using UnityEngine;
+
+namespace Runtime.Physic
+{
+    public class WheelWorldOffsetAdaptor : MonoBehaviour
+    {
+        [SerializeField] private WheelController wheel;
+
+        private void Awake()
+        {
+            WorldOffset.OnWorldOffsetChange += OffsetChanged;
+        }
+
+        private void OnDestroy()
+        {
+            WorldOffset.OnWorldOffsetChange -= OffsetChanged;
+        }
+
+        private void OffsetChanged(Vector3 offset)
+        {
+            wheel.CorrectReferencePosition(offset);
+        }
+    }
+}
